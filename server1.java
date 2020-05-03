5b9501e507ebb1171b82226614796825857fb7a1
rajeesh-coderraj <53893306+rajeesh-coderraj@users.noreply.github.com>
2020-05-03 19:17:27 +0530

    SD LAB

---
+++ server1.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+public class Server {
+public static void main(String
+args[]) throws Exception {
+ServerSocket ss = new
+ServerSocket(888);
+Socket s = ss.accept();
+System.out.println("Connection
+established");
+PrintStream ps = new
+PrintStream(s.getOutputStream());
+BufferedReader br = new
+BufferedReader(new
+InputStreamReader(s.getInputStream()));
+BufferedReader kb = new
+BufferedReader(new
+InputStreamReader(System.in));
+while (true) {
+String str, str1;
+
+while ((str =
+br.readLine()) != null) {
+System.out.println(str);
+str1 =
+kb.readLine();
+ps.println(str1);
+}
+
+
+ps.close();
+br.close();
+kb.close();
+ss.close();
+s.close();
+System.exit(0);
+}
+}
+}
